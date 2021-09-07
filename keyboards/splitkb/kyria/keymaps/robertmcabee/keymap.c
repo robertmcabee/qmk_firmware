237b6415527054d00632148eae11dd98ff782ed9
Robert McAbee <robertthemcabee@gmail.com>
2021-09-07 16:11:05 -0600

    change alpha layers

--- keyboards/splitkb/kyria/keymaps/robertmcabee/keymap.c
+++ keyboards/splitkb/kyria/keymaps/robertmcabee/keymap.c
@@ -34,10 +34,7 @@ enum layers {
 #define FKEYS    MO(_FUNCTION)
 #define ADJUST   MO(_ADJUST)
 
-#define CTL_ESC  MT(MOD_LCTL, KC_ESC)
-#define CTL_QUOT MT(MOD_RCTL, KC_QUOTE)
-#define CTL_MINS MT(MOD_RCTL, KC_MINUS)
-#define ALT_ENT  MT(MOD_LALT, KC_ENT)
+#define GUI_SPC  MT(MOD_LGUI, KC_SPACE)
 
 // Note: LAlt/Enter (ALT_ENT) is not the same thing as the keyboard shortcut Alt+Enter.
 // The notation `mod/tap` denotes a key that activates the modifier `mod` when held down, and
@@ -48,22 +45,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * Base Layer: QWERTY
  *
- * ,-------------------------------------------.                              ,-------------------------------------------.
- * |  Tab   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  Bksp  |
- * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "|
- * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |CapsLk|  |F-keys|  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |Adjust| LGUI | LAlt/| Space| Nav  |  | Sym  | Space| AltGr| RGUI | Menu |
- *                        |      |      | Enter|      |      |  |      |      |      |      |      |
- *                        `----------------------------------'  `----------------------------------'
+ *      ,-------------------------------------------.                                              ,-------------------------------------------.
+ *      |  Tab   |   Q  |   W  |   E  |   R  |   T  |                                              |   Y  |   U  |   I  |   O  |   P  |  Bksp  |
+ *      |--------+------+------+------+------+------|                                              |------+------+------+------+------+--------|
+ *      |   Esc  |   A  |   S  |   D  |   F  |   G  |                                              |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "|
+ *      |--------+------+------+------+------+------+-------------.                  ,-------------+------+------+------+------+------+--------|
+ *      | f16    |   Z  |   X  |   C  |   V  |   B  | [ {  |CapsLk|                  |F-keys|  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift |
+ *      `----------------------+------+------+------+------+------|                  |------+------+------+------+------+----------------------'
+ *                             |Adjust| LGUI | LAlt/| Space| Nav  |                  | Sym  | Space| AltGr| RGUI | Menu |
+ *                             |      |      | Enter|      |      |                  |      |      |      |      |      |
+ *                             `----------------------------------'                  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
-     CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                ADJUST , KC_LGUI, ALT_ENT, KC_SPC , NAV   ,     SYM    , KC_SPC ,KC_RALT, KC_RGUI, KC_APP
+     KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                         KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_F14 ,
+     KC_ESC  , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                         KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN, KC_ENT ,
+     KC_F16  , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LOPT,KC_F13 ,      KC_RGUI, KC_RCTL, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_QUOT, KC_F15,
+                                ADJUST , KC_LGUI, KC_BSPC, KC_LSFT, SYM   ,      NAV    , GUI_SPC ,KC_DEL, KC_RSFT, KC_CAPS
     ),
 
 
@@ -82,10 +79,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_COLEMAK_DH] = LAYOUT(
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_BSPC,
-     CTL_ESC , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_G ,                                        KC_M,   KC_N ,  KC_E ,   KC_I ,  KC_O , CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                 ADJUST, KC_LGUI, ALT_ENT, KC_SPC , NAV   ,     SYM    , KC_SPC ,KC_RALT, KC_RGUI, KC_APP
+     KC_TAB  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_F14,
+     KC_ESC  , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_G ,                                        KC_M,   KC_N ,  KC_E ,   KC_I ,  KC_O , KC_ENT,
+     KC_F16  , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_LOPT,KC_F13,     KC_RGUI, KC_RCTL,  KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_QUOT, KC_F15,
+                                ADJUST , KC_LGUI, KC_BSPC, KC_LSFT, SYM  ,     NAV    , GUI_SPC ,KC_DEL, KC_RSFT, KC_CAPS
     ),
 
 /*
