0fb4e52b58af38917b030c5fd431284619afed4d
Robert McAbee <robertthemcabee@gmail.com>
2021-09-10 23:28:27 -0600

    remove unused layers

--- keyboards/splitkb/kyria/keymaps/aNewKeymap/keymap.c
+++ keyboards/splitkb/kyria/keymaps/aNewKeymap/keymap.c
@@ -16,72 +16,32 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-    _QWERTY = 0,
-    _DVORAK,
-    _COLEMAK_DH,
+    _COLEMAK_DH = 0,
     _NAV,
     _SYM,
-    _FUNCTION,
     _ADJUST,
 };
 
 
 // Aliases for readability
-#define QWERTY   DF(_QWERTY)
-#define COLEMAK  DF(_COLEMAK_DH)
-#define DVORAK   DF(_DVORAK)
-
 #define SYM      MO(_SYM)
 #define NAV      MO(_NAV)
-#define FKEYS    MO(_FUNCTION)
 #define ADJUST   MO(_ADJUST)
 
-#define GUI_SPC  MT(MOD_LGUI, KC_SPACE)
-
-#define CTL_ESC  MT(MOD_LCTL, KC_ESC)
-#define CTL_QUOT MT(MOD_RCTL, KC_QUOTE)
-#define CTL_MINS MT(MOD_RCTL, KC_MINUS)
-#define ALT_ENT  MT(MOD_LALT, KC_ENT)
-
-// Note: LAlt/Enter (ALT_ENT) is not the same thing as the keyboard shortcut Alt+Enter.
-// The notation `mod/tap` denotes a key that activates the modifier `mod` when held down, and
-// produces the key `tap` when tapped (i.e. pressed and released).
+#define CMD_SPC  MT(MOD_LGUI, KC_SPACE)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/*
 
- */
-    [_QWERTY] = LAYOUT(
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_F14,
-     KC_ESC  , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_G ,                                        KC_M,   KC_N ,  KC_E ,   KC_I ,  KC_O , KC_ENT,
-     KC_F16  , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_LOPT,KC_F13,     KC_RGUI, KC_RCTL,  KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_QUOT, KC_F15,
-                                ADJUST , KC_LGUI, KC_BSPC, KC_LSFT, SYM  ,     NAV    , GUI_SPC ,KC_DEL, KC_RSFT, KC_CAPS
-    ),
 
-/*
-
- */
-    [_DVORAK] = LAYOUT(
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_F14,
-     KC_ESC  , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_G ,                                        KC_M,   KC_N ,  KC_E ,   KC_I ,  KC_O , KC_ENT,
-     KC_F16  , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_LOPT,KC_F13,     KC_RGUI, KC_RCTL,  KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_QUOT, KC_F15,
-                                ADJUST , KC_LGUI, KC_BSPC, KC_LSFT, SYM  ,     NAV    , GUI_SPC ,KC_DEL, KC_RSFT, KC_CAPS
-    ),
-
-/*
-
- */
     [_COLEMAK_DH] = LAYOUT(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_F14,
      KC_ESC  , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_G ,                                        KC_M,   KC_N ,  KC_E ,   KC_I ,  KC_O , KC_ENT,
      KC_F16  , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_LOPT,KC_F13,     KC_RGUI, KC_RCTL,  KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_QUOT, KC_F15,
-                                ADJUST , KC_LGUI, KC_BSPC, KC_LSFT, SYM  ,     NAV    , GUI_SPC ,KC_DEL, KC_RSFT, KC_CAPS
+                                ADJUST , KC_LGUI, KC_BSPC, KC_LSFT, SYM  ,     NAV    , CMD_SPC ,KC_DEL, KC_RSFT, KC_CAPS
     ),
 
-/*
 
- */
     [_NAV] = LAYOUT(
       _______, _______  , KC_F9  , KC_F10 , KC_F11 , KC_F12,                                     KC_F16, KC_UNDO, KC_UP,   KC_BSPC,  KC_PGUP, _______,
       _______, _______  , KC_F5  , KC_F6  , KC_F7  , KC_F8,                                      KC_F17, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL, _______,
@@ -89,9 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
-/*
 
- */
     [_SYM] = LAYOUT(
       KC_GRV , KC_AMPR, KC_BSLS, KC_LCBR, KC_RCBR, KC_DLR ,                                       KC_EQL ,  KC_7  ,  KC_8 , KC_9 , KC_PLUS, KC_QUOT,
      KC_TILD , KC_UNDS, KC_SLSH, KC_LPRN, KC_RPRN, KC_EXLM,                                       KC_QUES,  KC_4  ,  KC_5 , KC_6 , KC_MINS, KC_ASTR,
@@ -99,19 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  _______, _______, _______, _______, _______,   _______,   KC_0 ,   KC_0 , _______, _______
     ),
 
-/*
 
- */
-    [_FUNCTION] = LAYOUT(
-      _______,  KC_F9 ,  KC_F10,  KC_F11,  KC_F12, _______,                                     _______, _______, _______, _______, _______, _______,
-      _______,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 , _______,                                     _______, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI, _______,
-      _______,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-    ),
-
-/*
-
- */
     [_ADJUST] = LAYOUT(
       _______, _______, _______, _______ , _______, _______,                                      _______, _______, _______, _______,  _______, _______,
       _______, _______, _______, _______ , _______, _______,                                      RGB_TOG, RGB_SAI, RGB_HUI, RGB_VAI,  RGB_MOD, _______,
