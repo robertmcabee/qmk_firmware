f715227a3cb1b76c327c289c8c3c635b13f43ced
Robert McAbee <robertthemcabee@gmail.com>
2022-01-28 13:45:32 -0700

    misc. layout changes

--- keyboards/splitkb/kyria/keymaps/robertmcabee/keymap.c
+++ keyboards/splitkb/kyria/keymaps/robertmcabee/keymap.c
@@ -24,21 +24,24 @@ enum layers {
 
 
 // Aliases for readability
-#define SYM      MO(_SYM)
-#define NAV      MO(_NAV)
-#define ADJUST   MO(_ADJUST)
+#define SYM      OSL(_SYM)
+#define NAV      OSL(_NAV)
+#define ADJUST   OSL(_ADJUST)
 
 #define CMD_SPC  MT(MOD_LGUI, KC_SPACE)
+
+#define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 3000  /* Time (in ms) before the one shot key is released */
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     [_COLEMAK_DH] = LAYOUT(
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_F14,
-     KC_ESC  , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_G ,                                        KC_M,   KC_N ,  KC_E ,   KC_I ,  KC_O , KC_QUOT,
-     KC_F16  , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_LOPT,KC_F13,     KC_RGUI, KC_RCTL,  KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_SLSH, KC_F15,
-                                ADJUST , KC_LGUI, KC_BSPC, KC_LSFT, SYM  ,     NAV    , CMD_SPC,KC_DEL, KC_ENT , KC_CAPS
+     KC_HOME , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_EQL ,
+     KC_TAB  , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_G ,                                        KC_M,   KC_N ,  KC_E ,   KC_I ,  KC_O , KC_QUOT,
+     KC_CAPS , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_LOPT,KC_F13,     KC_CAPS, KC_LCTL,  KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_SLSH, KC_BSLS,
+                                ADJUST , KC_LGUI, KC_BSPC, KC_LSFT, SYM  ,     NAV    , CMD_SPC,KC_DEL, KC_ENT , KC_END
     ),
 
 
@@ -51,9 +54,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     [_SYM] = LAYOUT(
-      KC_GRV , KC_AMPR, KC_BSLS, KC_LCBR, KC_RCBR, KC_DLR ,                                       KC_EQL ,  KC_7  ,  KC_8 , KC_9 , KC_PLUS, KC_QUOT,
-     KC_TILD , KC_UNDS, KC_SLSH, KC_LPRN, KC_RPRN, KC_EXLM,                                       KC_QUES,  KC_4  ,  KC_5 , KC_6 , KC_MINS, KC_ASTR,
-     KC_PIPE , KC_HASH, KC_COLN, KC_LBRC, KC_RBRC, KC_DQUO, _______, _______,   _______, _______, KC_AT  ,  KC_1  ,  KC_2 , KC_3 , KC_PERC, KC_CIRC,
+      _______, _______, KC_EXLM, KC_LCBR, KC_RCBR, KC_TILD,                                       KC_ASTR,  KC_7  ,  KC_8 , KC_9 , KC_COLN, KC_PLUS,
+      _______, KC_AMPR, KC_DLR , KC_LPRN, KC_RPRN, KC_GRV ,                                       KC_MINS,  KC_4  ,  KC_5 , KC_6 , KC_HASH, KC_DQUO,
+      _______, KC_AT  , KC_PERC, KC_LBRC, KC_RBRC, KC_CIRC, _______, _______,   _______, _______, KC_UNDS,  KC_1  ,  KC_2 , KC_3 , KC_QUES, KC_PIPE,
                                  _______, _______, _______, _______, _______,   _______, _______ ,  KC_0 , _______, _______
     ),
 
--- keyboards/splitkb/kyria/keymaps/robertmcabee/readme.md
+++ keyboards/splitkb/kyria/keymaps/robertmcabee/readme.md
@@ -1,5 +1,12 @@
-# Keymap
+# Readme
+![Keymap](https://i.ibb.co/k16L2Zj/Screen-Shot-2022-01-28-at-1-36-19-PM.png)
+\
+This is my keymap.
 
-![Keymap](https://i.ibb.co/XZvTt6c/whatKey.png)
+- Colemak-DH layout
+- Space and shift on home thumb postitions.
+- Option, Cmd, Shift & Ctrl are all usable with thumbs.
+- Standard numpad layout to utilize existing muscle memory
+- One-Shot Layers
 
-
+*"Hyper Esc" shown on keymap is actually KC_CAPS - It works for my setup, don't @ me.*
